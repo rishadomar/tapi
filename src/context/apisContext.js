d8815ac24fe11345d78212fd32e8184d515c6f2e
Rishad Omar <rishad@aux.co.za>
2021-10-15 16:55:36 +0200

    Compare expected to received.

--- src/context/apisContext.js
+++ src/context/apisContext.js
@@ -1,10 +1,14 @@
 import { createContext, useState } from 'react';
 import * as ProjectConstants from 'projectConstants';
+import Settings from 'data/settings.json';
 export const ApiContext = createContext();
 
 const ApiProvider = ({ children }) => {
     const [apis, setApis] = useState(null);
 
+    /**
+     * Read all API entries
+     */
     const readApis = () => {
         fetch(ProjectConstants.ALL_APIS_FILE, {
             headers: {
@@ -28,6 +32,11 @@ const ApiProvider = ({ children }) => {
             });
     };
 
+    /**
+     * Update an entry
+     * @param {*} updatedApiEntry
+     * @returns
+     */
     const updateApiEntry = (updatedApiEntry) => {
         if (apis === null) {
             return;
@@ -43,20 +52,88 @@ const ApiProvider = ({ children }) => {
         });
     };
 
-    // const getAllApis = () => {
-    //     return apis;
-    // };
+    /**
+     * Check if there is a diff
+     * Copied from: https://stackoverflow.com/questions/201183/how-to-determine-equality-for-two-javascript-objects/16788517#16788517
+     * @param {*} entry
+     */
+    const objectEquals = (x, y) => {
+        // optional
+        if (x === '?') {
+            return true;
+        }
 
-    const getApiDetail = (i) => {
-        return apis[i];
+        if (x === null || x === undefined || y === null || y === undefined) {
+            return x === y;
+        }
+
+        // don't care what the value is
+        if (x === '*') {
+            return true;
+        }
+
+        // after this just checking type of one would be enough
+        if (x.constructor !== y.constructor) {
+            return false;
+        }
+        // if they are functions, they should exactly refer to same one (because of closures)
+        if (x instanceof Function) {
+            return x === y;
+        }
+        // if they are regexps, they should exactly refer to same one (it is hard to better equality check on current ES)
+        if (x instanceof RegExp) {
+            return x === y;
+        }
+        if (x === y || x.valueOf() === y.valueOf()) {
+            return true;
+        }
+        if (Array.isArray(x) && x.length !== y.length) {
+            return false;
+        }
+
+        // if they are dates, they must had equal valueOf
+        if (x instanceof Date) {
+            return false;
+        }
+
+        // if they are strictly equal, they both need to be object at least
+        if (!(x instanceof Object)) {
+            return false;
+        }
+        if (!(y instanceof Object)) {
+            return false;
+        }
+
+        // recursive object equality check
+        var p = Object.keys(x);
+        return (
+            Object.keys(y).every(function (i) {
+                return p.indexOf(i) !== -1;
+            }) &&
+            p.every(function (i) {
+                return objectEquals(x[i], y[i]);
+            })
+        );
     };
 
+    /**
+     * Execute the API
+     * @param {*} entry
+     */
     const executeApi = (entry) => {
-        console.log('Execute the api: ', entry);
+        console.log('Execute the api: ', entry, Settings);
         let newEntryDetails = { ...entry };
         newEntryDetails.status = 'Busy';
 
-        fetch(entry.api, {
+        //
+        // Replace the Settings values
+        //
+        let url = entry.api;
+        for (var key of Object.keys(Settings)) {
+            url = url.replace('${' + key + '}', Settings[key]);
+        }
+
+        fetch(url, {
             method: entry.method,
         })
             .then(function (response) {
@@ -69,8 +146,15 @@ const ApiProvider = ({ children }) => {
             })
             .then(function (myJson) {
                 newEntryDetails.executeResult = myJson;
-                newEntryDetails.status = 'Success';
-                updateApiEntry(newEntryDetails);
+
+                if (objectEquals(entry.expectedResponse, myJson)) {
+                    newEntryDetails.status = 'Success';
+                    console.log('Same: ', entry.expectedResponse, myJson);
+                } else {
+                    newEntryDetails.status = 'Diff';
+                    console.log('Different: ', entry.expectedResponse, myJson);
+                }
+                setTimeout(() => updateApiEntry(newEntryDetails), 3000);
             })
             .catch((error) => {
                 console.log('Error encountered. ', error.message);
@@ -81,9 +165,7 @@ const ApiProvider = ({ children }) => {
     };
 
     return (
-        <ApiContext.Provider
-            value={{ readApis, apis, getApiDetail, executeApi }}
-        >
+        <ApiContext.Provider value={{ readApis, apis, executeApi }}>
             {children}
         </ApiContext.Provider>
     );
