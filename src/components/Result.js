dd05c186d7c50c54476a74f0f3a2e899177c1df2
Rishad Omar <rishad@aux.co.za>
2021-10-15 16:55:08 +0200

    Added badge

--- src/components/Result.js
+++ src/components/Result.js
@@ -10,6 +10,12 @@ const Result = ({ success }) => {
     if (success === 'Failed') {
         return <span className="badge badge-danger-400 mr-3">{success}</span>;
     }
+
+    if (success === 'Diff') {
+        return <span className="badge badge-warning-400 mr-3">{success}</span>;
+    }
+
+    return <span className="badge badge-info-400 mr-3">{success}</span>;
 };
 
 export default Result;
